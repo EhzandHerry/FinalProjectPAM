7eea350be6a8b1ad5c92df535b10839194d97277
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-01-08 03:01:27 +0700

    buat file object untuk SumberDataAlatMusik

---
+++ app/src/main/java/com/example/finalprojectpam/data/SumberDataAlatMusik.kt
@@ -0,0 +1,13 @@
+package com.example.finalprojectpam.data
+
+import com.example.finalprojectpam.R
+
+object SumberDataAlatMusik {
+    val alatmusik = listOf(
+        R.string.gitar,
+        R.string.piano,
+        R.string.biola,
+        R.string.cello,
+        R.string.ukulele
+    )
+}
