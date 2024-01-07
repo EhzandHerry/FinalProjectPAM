eff7f6f6ad137ccb85cd01aeb86f070c5da35280
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-01-08 04:08:49 +0700

    edit interface AppContainer di KontainerPemesanan.kt

--- app/src/main/java/com/example/finalprojectpam/data/KontainerPemesanan.kt
+++ app/src/main/java/com/example/finalprojectpam/data/KontainerPemesanan.kt
@@ -1,10 +1,12 @@
 package com.example.finalprojectpam.data
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 interface AppContainer {
-    val RepositoriPemesanan: RepositoriPemesanan
+    val repositoriPemesanan: RepositoriPemesanan
 }
 
-class KontainerPemesanan : AppContainer{
+class KontainerPemesanan: AppContainer{
     private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override val repositoriPemesanan: RepositoriPemesanan by lazy {
