31ead74f6d13973674d7000f63666540b3ca7688
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-01-09 03:18:38 +0700

    buat composable function DataPemesanan di HomeScreen pemesanan

--- app/src/main/java/com/example/finalprojectpam/ui/pemesanan/home/HomeScreen.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/pemesanan/home/HomeScreen.kt
@@ -1,2 +1,62 @@
 package com.example.finalprojectpam.ui.pemesanan.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.finalprojectpam.navigasi.DestinasiNavigasi
+
+object DestinasiHomePemesanan : DestinasiNavigasi {
+    override val route = "Home Pemesanan"
+    override val titleRes = "Pemesanan"
+}
+
+@Composable
+fun DataPemesanan(
+    pemesanan: Pemesanan,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = pemesanan.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = alatMusik.jenis,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = alatMusik.harga,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
