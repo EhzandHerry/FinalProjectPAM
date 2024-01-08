6910c2b736184b250bff0a874d77f8e40e2d3e1e
EhzandHerry <ehzand.herry1926@gmail.com>
2024-01-08 15:23:52 +0700

    tambah scafffold di HomeScreen.kt

--- app/src/main/java/com/example/finalprojectpam/ui/Home/HomeScreen.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/Home/HomeScreen.kt
@@ -1,8 +1,18 @@
 package com.example.finalprojectpam.ui.Home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalprojectpam.navigasi.DestinasiNavigasi
 import com.example.finalprojectpam.ui.PenyediaViewModel
@@ -13,10 +23,39 @@ object destinasiHome : DestinasiNavigasi {
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
+
 fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
     onDetailClick: (String) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+                )
+    {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = ""
+                )
+            }
+        }
+    ) { innerPadding ->
+        val uiStateSiswa by viewModel.homeUIState.collectAsState()
+        BodyHome(
+            itemKontak = uiStateSiswa.listKontak,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            onSiswaClick = onDetailClick
+        )
+    }
+
+
+}
