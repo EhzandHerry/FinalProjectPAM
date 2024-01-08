2f5e1d62c3f27604cbb0372013dc2bfdf3328c62
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-01-08 19:10:49 +0700

    tambah data class DetailUIState dan function toDetailPemesanan di UIState.kt

--- app/src/main/java/com/example/finalprojectpam/ui/UIState.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/UIState.kt
@@ -25,7 +25,21 @@ fun AddEvent.toPemesanan()= Pemesanan(
     harga = harga,
     jumlah= jumlah,
     alat= alat
-
 )
+
+data class  DetailUIState(
+    val addEvent: AddEvent = AddEvent()
+)
+
+fun Pemesanan.toDetailPemesanan():AddEvent=
+    AddEvent(
+        id = id,
+        nama = nama,
+        nohp = nohp,
+        alamat = alamat,
+        harga = harga,
+        jumlah= jumlah,
+        alat= alat
+    )
 
 
