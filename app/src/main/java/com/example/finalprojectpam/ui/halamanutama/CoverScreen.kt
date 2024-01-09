de6d0ec538a8569102356bdb16a535c8c0bb612a
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-01-09 14:28:35 +0700

    buat composable function untuk CoverScreen.kt

--- app/src/main/java/com/example/finalprojectpam/ui/halamanutama/CoverScreen.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/halamanutama/CoverScreen.kt
@@ -1,8 +1,14 @@
 package com.example.finalprojectpam.ui.halamanutama
 
+import androidx.compose.runtime.Composable
 import com.example.finalprojectpam.navigasi.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "Cover"
     override val titleRes = "Pilih"
 }
+
+@Composable
+fun HalamanHomeCoverScreen(
+    onNextButtonClicked: () -> Unit
+) {}
