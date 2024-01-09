1f85677bd127143b984d4cf1853e7ff18c42305e
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-01-09 14:52:03 +0700

    tambah column dan text di CoverScreen.kt

--- app/src/main/java/com/example/finalprojectpam/ui/halamanutama/CoverScreen.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/halamanutama/CoverScreen.kt
@@ -4,12 +4,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.finalprojectpam.R
 import com.example.finalprojectpam.navigasi.DestinasiNavigasi
 
@@ -34,6 +39,18 @@ fun CoverScreen(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(vertical = 16.dp)
+        ){
+            Text(
+                text = stringResource(id = R.string.Jikustik),
+                color = Color.DarkGray,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
     }
 }
