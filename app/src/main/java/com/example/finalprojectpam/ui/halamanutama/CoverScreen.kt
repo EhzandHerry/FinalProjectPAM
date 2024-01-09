5aa1a61f20f2fecec10bf7f34887bfb2785ff293
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-01-09 14:46:24 +0700

    buat val image dan column untuk CoverScreen.kt

--- app/src/main/java/com/example/finalprojectpam/ui/halamanutama/CoverScreen.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/halamanutama/CoverScreen.kt
@@ -1,14 +1,39 @@
 package com.example.finalprojectpam.ui.halamanutama
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.finalprojectpam.R
 import com.example.finalprojectpam.navigasi.DestinasiNavigasi
 
+
+/**
 object DestinasiHome : DestinasiNavigasi {
     override val route = "Cover"
     override val titleRes = "Pilih"
-}
+}*/
 
+
+@Preview
 @Composable
-fun HalamanHomeCoverScreen(
-    onNextButtonClicked: () -> Unit
-) {}
+fun CoverScreen(
+  //  onNextButtonClicked: () -> Unit
+) {
+    val image = painterResource(id = R.drawable.cover)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(14.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
+}
