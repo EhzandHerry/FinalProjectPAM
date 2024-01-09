7e9d396c291f571f9ab87e899a01f4b9f9b736c6
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-01-10 00:08:18 +0700

    tambah file dimens.xml

--- app/src/main/java/com/example/finalprojectpam/ui/halamanutama/CoverScreen.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/halamanutama/CoverScreen.kt
@@ -1,20 +1,27 @@
 package com.example.finalprojectpam.ui.halamanutama
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 //import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,40 +40,60 @@ fun CoverScreen(
 ) {
     val image = painterResource(id = R.drawable.cover)
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(14.dp),
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(vertical = 16.dp)
-        ){
-            Text(
-                text = stringResource(id = R.string.Jikustik),
-                color = Color.White,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
+        modifier = Modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        OutlinedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+            ),
+            border = BorderStroke(1.dp, Color.Black), modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .padding(vertical = 55.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
-            )
+            ) {
+                Text(
+                    text = "Toko Alat Musik",
+                    color = Color.DarkGray,
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier =  Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    text = "Jikustik",
+                    color = Color.DarkGray,
+                    fontFamily = FontFamily.Serif,
+                    fontSize = 60.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+
+            }
         }
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
+        Row (
             modifier = Modifier
                 .fillMaxWidth()
-                .height(150.dp)
-        )
-        Button(
-            onClick = onNextButtonClicked,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp)
-        ) {
-            Text("Lanjut")
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .weight(3f, false),
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom
+        ){
+            Button(modifier = Modifier.weight(1f),
+                onClick = onNextButtonClicked
+            ) {
+                Text(stringResource(R.string.next))
+            }
         }
     }
 }
